1025bc0c46aef9736da7ef58a334c583f518ae47
Dea Dania <dea.dania.business@gmail.com>
2021-08-23 21:05:18 +0700

    unit test check url path with pytest done

--- personaldata/test_views.py
+++ personaldata/test_views.py
@@ -1,4 +1,40 @@
 import pytest
 
-def test_example():
-    assert  1 == 1
+from rest_framework.authtoken.models import Token
+from rest_framework.test import APIClient
+from rest_framework import status
+from django.contrib.auth.models import User
+from django.urls import reverse
+
+@pytest.mark.django_db
+def client():
+    usert = User.objects.get(id=1)
+    token = Token.objects.get(user=usert)
+    client = APIClient()
+    client.credentials(HTTP_AUTHORIZATION='Token ' + token.key)
+    return client
+
+def test_path_get_variable(client):
+    response = client.get(reverse('variable'))
+    assert response.request['PATH_INFO'] == '/variable/'
+
+def test_path_get_course(client):
+    response = client.get(reverse('course'))
+    assert response.request['PATH_INFO'] == '/course/'
+
+def test_path_get_experience(client):
+    response = client.get(reverse('experience'))
+    assert response.request['PATH_INFO'] == '/experience/'
+
+def test_path_get_education(client):
+    response = client.get(reverse('education'))
+    assert response.request['PATH_INFO'] == '/education/'
+
+def test_path_get_social(client):
+    response = client.get(reverse('social'))
+    assert response.request['PATH_INFO'] == '/social/'
+
+def test_path_get_about(client):
+    response = client.get(reverse('about'))
+    assert response.request['PATH_INFO'] == '/about/'
+

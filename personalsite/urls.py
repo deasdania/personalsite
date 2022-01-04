c1e41f7f686d2f428ce4e6fca1d0f2ab67ad9439
dea dania <deadania@deas-MacBook-Pro.local>
2022-01-04 07:34:26 +0700

    #comment

--- personalsite/urls.py
+++ personalsite/urls.py
@@ -19,6 +19,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 from personaldata import views
 
+# patterns
 urlpatterns = [
     path('', views.index, name='index'),
     path('admin/', admin.site.urls),
